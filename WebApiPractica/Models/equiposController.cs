6129e7bfbdba68a4b09b8353e8f3931663073a2a
DARWIN PROYECTOS <darwin.aguirre1@catolica.edu.sv>
2025-02-02 18:52:29 -0600

    Agregar archivos de proyecto.

---
+++ WebApiPractica/Models/equiposController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace WebApiPractica.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+
+
+
+        ////EndPoint que retorna el listado  de todos  los equipos existentes
+        ///
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos
+                                          select e).ToList();
+
+            if (listadoEquipo.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoEquipo);
+        }
+
+    }
+}
